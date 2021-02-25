588049dedb86abbc198c63ac9ce57aecc36f4600
LaurenGifford <lauren.gifford.17@gmail.com>
2021-02-25 16:46:57 -0500

    complete

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,19 @@
 import React from "react";
+import QuestionItem from "./QuestionItem"
 
-function QuestionList() {
+function QuestionList({questions, onDeleteQuestion}) {
+
+  const displayQuestions = questions.map(question => (
+    <QuestionItem 
+      question={question}
+      onDeleteQuestion={onDeleteQuestion}
+    />
+  ))
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{displayQuestions}</ul>
     </section>
   );
 }
